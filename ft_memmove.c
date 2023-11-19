1301f5abb656634f38dcf21129b5767bc8f2b24f
Matus Gavornik <mgavorni@c1r1s3.42prague.com>
2023-11-19 16:34:26 +0100

    memmove and headers

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,33 +1,29 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matus <matus@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:45:30 by mgavorni          #+#    #+#             */
-/*   Updated: 2023/11/17 18:33:54 by matus            ###   ########.fr       */
+/*   Updated: 2023/11/19 16:33:27 by mgavorni         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char		*destination;
-	const char	*source;
 	size_t		i;
 
-	destination = (char *)dest;
-	source = (const char *)src;
 	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	if (destination > source && destination < source + n)
+	if (dest > src && dest < src + n)
 	{
 		while (n > 0)
 		{
-			destination[n - 1] = source[n - 1];
+			((unsigned char *)dest)[n - 1] = ((unsigned char *)src)[n - 1];
 			n--;
 		}
 	}
@@ -35,7 +31,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		while (i < n)
 		{
-			destination[i] = source[i];
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 			i++;
 		}
 	}
